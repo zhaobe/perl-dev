d998d64cd9c5813cb38fb57ac3f8fd616e937358
zhaobe <zhaoben777@gmail.com>
2017-05-30 23:31:55 -0700

    Add escaping characters with strings example.

---
+++ 4_escapeChar.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+$var = "This is outside, \"this is inside the double quotes\"";
+
+print "Output is the string stored in var:\n";
+print "$var\n\n";
+
+print "Output prints the word var since it is escaped with backslash:\n";
+print "\$var\n";
