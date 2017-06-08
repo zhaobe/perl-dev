d005b7fe037cf12ae2815c3071a65668bce96bca
zhaobe <zhaoben777@gmail.com>
2017-06-07 23:59:34 -0700

    Adding scalars example.

---
+++ 7_scalars.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+print "Numeric Scalar";
+$integer = 100;
+$octal = 0377;
+$hexa = 0xff;
+
+print "integer = $integer\n";
+print "octal = $octal\n";
+print "hexadecimal = $hexidecimal\n";
