add05982d833407c02d8c5506ebebba363768b88
zhaobe <zhaoben777@gmail.com>
2017-06-11 23:53:13 -0700

    Making changes to arrays example, commented error of first array, added use strict.

--- 8_arrays.pl
+++ 8_arrays.pl
@@ -1,10 +1,20 @@
 #!/usr/bin/perl
 
-# using the qw operator
-@array = (1, 2, 'Hello');
-@array = qw/This is an array/;
+use strict;
+use warnings;
+
+# using the qw operator, it acts similar to parentheses
+# my @array = (1, 2, 'Hello');
+my @array = qw/This is an array/;
 
 print "$array[0]\n";
 print "$array[1]\n";
 print "$array[2]\n";
 print "$array[3]\n";
+
+# interesting way to access elements
+print "unique way of accessing array, should be:\n";
+print "array[-1] = $array[-1]\n";
+print "array[-4] = $array[-4]\n";
+print "array[-8] = $array[-8]\n";
+
