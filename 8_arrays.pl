c68675a7d3d040c3be7f6fa5765b69306a570551
zhaobe <zhaoben777@gmail.com>
2017-06-15 01:23:12 -0700

    Updating with sequential number array, cannot use a..z since it is a bareword when using use strict.

--- 8_arrays.pl
+++ 8_arrays.pl
@@ -6,15 +6,23 @@ use warnings;
 # using the qw operator, it acts similar to parentheses
 # my @array = (1, 2, 'Hello');
 my @array = qw/This is an array/;
+print "Our string is: @array\n";
 
-print "$array[0]\n";
-print "$array[1]\n";
-print "$array[2]\n";
-print "$array[3]\n";
+print "index 1 is: $array[0]\n";
+print "index 2 is: $array[1]\n";
+print "index 3 is: $array[2]\n";
+print "index 4 is: $array[3]\n";
 
-# interesting way to access elements
-print "unique way of accessing array, should be:\n";
+# interesting way to access elements, can access indexes
+# with negative values, until it hits a second repeat of
+# the string.
+print "\n\Uunique way of accessing array elements:\n";
 print "array[-1] = $array[-1]\n";
 print "array[-4] = $array[-4]\n";
+
+print "\nThis next example should error out\n";
 print "array[-8] = $array[-8]\n";
 
+print "\n\Usequential number array examples:\n";
+my @tenToTwenty = (10..20);
+print "@tenToTwenty\n";
