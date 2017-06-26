624750d49ed2088ffff6ffaacd07173fa5c6bf50
zhaobe <zhaoben777@gmail.com>
2017-06-25 23:30:05 -0700

    Added slicing array elements example.

--- 9_arrays2.pl
+++ 9_arrays2.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/perl
 
-print "\Uadding and removing elements in array\n";
+use strict;
+use warnings;
+
+print "\U-----adding and removing elements in array-----\n";
 my @nums = ("one", "two", "three");
 print "\@nums = @nums \n\n";
 
@@ -19,4 +22,12 @@ print "\@nums = @nums \n\n";
 print "remove one element from the beginning\n";
 shift(@nums);
 print "\@nums = @nums \n\n";
+
+print "\U-----slicing array elements-----\n";
+my @days = qw/Mon Tue Wed Thu Fri Sat Sun/;
+my @weekdays = @days[2, 3, 4, 5];
+my @weekdays2 = @days[2..5];
+
+print "regular array: \t\t@weekdays\n";
+print "array with .. range: \t@weekdays2\n"
 
