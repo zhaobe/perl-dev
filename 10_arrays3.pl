da52008332167e2fd1cb60dbf3ba9a3c9593f21e
zhaobe <zhaoben777@gmail.com>
2017-06-29 00:06:12 -0700

    Adding string transformation to array and vice versa.

--- 10_arrays3.pl
+++ 10_arrays3.pl
@@ -9,3 +9,17 @@ print "Initial array: @array\n";
 
 splice(@array, 15, 5, 50..54);
 print "After splice: @array\n";
+
+print "\U-----transform strings to array-----\n";
+my $string = "this is a string that will be with spaces";
+print "the original string is: \n";
+print "$string\n\n";
+
+my @string_arr = split(' ', $string);
+print "fourth index is: $string_arr[4]\n";
+print "sixth index is: $string_arr[6]\n\n";
+
+print "\U-----transform array to string-----\n";
+# reusing the same array split from above
+my $join_string = join(' ', @string_arr);
+print "$join_string\n\n";
