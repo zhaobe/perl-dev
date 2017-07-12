5081aa837f3d337897c0571ab09c492c5379d364
zhaobe <zhaoben777@gmail.com>
2017-07-11 22:16:50 -0700

    Fixed the extracting values only issue, added checking existence and getting hash size.

--- 9_hashes.pl
+++ 9_hashes.pl
@@ -4,6 +4,7 @@ use strict;
 
 print "\U-----create and access hash elements-----\n";
 my %hash = ('Alice', 10, 'Bob', 15, 'Cathy', 20);
+#my %hash = ('Alice' => 10, 'Bob' => 15, 'Cathy' => 20);
 
 print "\$hash{'Alice'} = $hash{'Alice'}\n";
 print "\$hash{'Bob'} = $hash{'Bob'}\n";
@@ -21,7 +22,22 @@ print "$names[2]\n";
 
 print "\U-----extracting values only-----\n";
 my @ages = values %hash;
-print "$names[0]\n";
-print "$names[1]\n";
-print "$names[2]\n";
+print "$ages[0]\n";
+print "$ages[1]\n";
+print "$ages[2]\n";
 
+print "\U-----check for existence-----\n";
+if( exists($hash{'Bob'}) ){
+    print "Bob is $hash{'Bob'} years old\n";
+}
+else {
+    print "I don't know the age of Bob.\n";
+}
+print "\U-----getting hash size-----\n";
+my @keys = keys %hash;
+my $size = @keys;
+print "hash size for keys is: $size\n";
+
+my @values = values %hash;
+my $size = @values;
+print "hash size for values is: $size\n";
