276b3a7817df0b723b09282bc539349101f25e0a
zhaobe <zhaoben777@gmail.com>
2017-07-16 00:08:42 -0700

    Made slight formatting to output for date and times.

--- 10_date_time.pl
+++ 10_date_time.pl
@@ -6,21 +6,25 @@
 @months = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
 @days = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
 
-print "\nOne way to display localtime\n";
+print "\nOne way to display localtime:\n";
 ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
-print "$mday $months[$mon] $days[$wday]\n";
+print "\t$mday $months[$mon] $days[$wday]\n";
 
-print "\nSecond way to display localtime\n";
+print "\nSecond way to display localtime:\n";
 $date_time = localtime();
-print "Local date and time is: $date_time\n\n";
+print "\t$date_time\n\n";
 
 $gmt = gmtime();
-print "GMT date and time is: $gmt\n";
+print "GMT date and time is:\n"; 
+print "\t$gmt\n\n";
 
 $epoc = time();
-print "Epoch time, seconds since Jan 1 1970: $epoc\n\n";
+print "Epoch time, seconds since Jan 1 1970:\n"; 
+print "\t$epoc\n\n";
 
-print "Get yesterday's date and time using epoch\n";
+print "Get yesterday's date and time using time() and localtime(),
+the time() function gets the epoch time:\n";
+
 $epoc = $epoc - 12 * 60 * 60;
 $date_time = localtime($epoc);
-print "Yesterday's date and time is: $date_time"; # reused the date_time var
+print "\t$date_time\n\n"; # reused the date_time var
